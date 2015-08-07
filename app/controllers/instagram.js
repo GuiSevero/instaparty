1d37f9732a6bf8440f520e3721ef66acb7f3096b
Guilherme Severo <guilherme.severo@huia.com.br>
2015-08-07 14:00:56 -0300

    adicionado client secret

--- app/controllers/instagram.js
+++ app/controllers/instagram.js
@@ -112,12 +112,13 @@ exports.get_subscribe = function(req, res) {
 
 
 exports.post_subscribe = function(request, response) {
+
   if(!request.body){
     console.log("Error 400 - Body not ok");
+    console.log(request.body);
     return send(400, body);
   }
 
-  var canBroadCast = false;
 
     var log = new Log({
         data: request.body
@@ -134,7 +135,8 @@ exports.post_subscribe = function(request, response) {
         var url_path = '/v1/' + pluralize(notificationOjb.object) + '/' + notificationOjb.object_id + '/media/recent' +
             '?' + querystring.stringify({
                 client_id: config.instagram.client_id,
-                count: 3
+                client_secret: config.instagram.client_secret,
+                count: 1
             });
       console.log(url_path);
         https.get({
@@ -180,8 +182,7 @@ exports.post_subscribe = function(request, response) {
                                     if (err) {
                                         console.log(err);
                                     } else {
-                                        canBroadCast = true;
-                                        console.log(instagram_notif);
+                                        global.io.sockets.emit('photo', instagram_notif);
                                     }
                                 });
 
@@ -189,10 +190,6 @@ exports.post_subscribe = function(request, response) {
                         });
 
                     } //endfor
-
-                    //Notifica que há novas fotos
-                    if(canBroadCast)
-                      global.io.sockets.emit('photo', instagram_notif);
                 }
             });
 
